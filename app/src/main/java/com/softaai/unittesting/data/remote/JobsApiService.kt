bf2ee1d7a734d153aabad1e4c928a2f864e88a8a
amol pawar <amoljp19@gmail.com>
2021-06-20 01:06:06 +0530

    added GitHub Jobs deprecated comment, jobs list not showing on ui, not getting any response from api end point.

--- app/src/main/java/com/softaai/unittesting/data/remote/JobsApiService.kt
+++ app/src/main/java/com/softaai/unittesting/data/remote/JobsApiService.kt
@@ -1,11 +1,13 @@
 package com.softaai.unittesting.data.remote
 
-import com.softaai.unittesting.model.JobsApiResponse
 import com.softaai.unittesting.model.JobsItemApiResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
 interface JobsApiService {
+
+    /*GitHub Jobs is deprecated! New jobs will not be posted from May 19, 2021. It will shut down entirely on August 19, 2021.
+    Read more in the GitHub blog post.*/
 
     @GET("/positions.json?description=marketing")
     suspend fun getJobs(): Response<List<JobsItemApiResponse>>
