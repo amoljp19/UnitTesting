2f015a8df8dbfdeb1e326904bad6d5794b57e32f
amol pawar <amoljp19@gmail.com>
2021-06-19 23:48:08 +0530

    added recyclerview and item item layout in xml file also added coil dependency

--- app/src/main/java/com/softaai/unittesting/view/MainActivity.kt
+++ app/src/main/java/com/softaai/unittesting/view/MainActivity.kt
@@ -2,9 +2,16 @@ package com.softaai.unittesting.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.softaai.unittesting.R
+import com.softaai.unittesting.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    val mViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
