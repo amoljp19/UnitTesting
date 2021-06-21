1ab4925d9007d3e77a2c174aef536d49ffad0e5d
amol pawar <amoljp19@gmail.com>
2021-06-21 11:51:35 +0530

    passed testIfEmailAndPasswordValid_DoLogin() and throwErrorOnFailure

--- app/src/test/java/com/softaai/unittesting/jobs/login/LoginViewModelTest.kt
+++ app/src/test/java/com/softaai/unittesting/jobs/login/LoginViewModelTest.kt
@@ -2,22 +2,32 @@ package com.softaai.unittesting.jobs.login
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.softaai.unittesting.data.remote.Resource
 import com.softaai.unittesting.data.repository.LoginUserRepository
 import com.softaai.unittesting.jobs.CoroutineTestRule
 import com.softaai.unittesting.jobs.login.util.LoginDataState
 import com.softaai.unittesting.jobs.login.util.LoginValidator
+import com.softaai.unittesting.model.JobsItemApiResponse
+import com.softaai.unittesting.model.LoginUser
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import org.mockito.ArgumentMatchers
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 import org.powermock.core.classloader.annotations.PrepareForTest
 import org.powermock.modules.junit4.PowerMockRunner
 
@@ -31,6 +41,8 @@ class LoginViewModelTest {
     @Mock
     private lateinit var loginUserRepository: LoginUserRepository
 
+    @Mock
+    private lateinit var loginUser: LoginUser
 
     private val mockObserverForStates = mock<Observer<LoginDataState>>()
 
@@ -82,6 +94,58 @@ class LoginViewModelTest {
 
         //Assert
         verify(mockObserverForStates).onChanged(LoginDataState.InValidPasswordState)
+        verifyNoMoreInteractions(mockObserverForStates)
+    }
+
+
+    @Test
+    fun testIfEmailAndPasswordValid_DoLogin() {
+         // Arrange
+        `when`(LoginValidator.isEmailValid(anyString())).thenAnswer { true }
+        `when`(LoginValidator.isPasswordValid(anyString())).thenAnswer { true }
+
+        val data = mock<LoginUser>()
+
+        runBlockingTest {
+            whenever(loginUserRepository.getLoginUserByCredentials(anyString(), anyString())) doReturn flowOf(
+                data
+            )
+        }
+
+        //Act
+        loginViewModel.doLogin(EMAIL, PASSWORD)
+
+        //Assert
+        verify(mockObserverForStates).onChanged(LoginDataState.ValidCredentialsState)
+        verify(
+            mockObserverForStates, times(2)
+        ).onChanged(LoginDataState.Success(ArgumentMatchers.any()))
+        verifyNoMoreInteractions(mockObserverForStates)
+    }
+
+    @Test
+    fun testThrowError_OnLoginFailed() {
+        //Arrange
+        `when`(LoginValidator.isEmailValid(anyString())).thenAnswer { true }
+        `when`(LoginValidator.isPasswordValid(anyString())).thenAnswer { true }
+
+        // here we can assume any kind of exception, due to that login fails
+        val error = RuntimeException()
+
+        runBlocking {
+            `when`(loginUserRepository.getLoginUserByCredentials(anyString(), anyString())).thenThrow(
+                error
+            )
+        }
+
+        //Act
+        loginViewModel.doLogin(EMAIL, PASSWORD)
+
+
+        //Assert
+        verify(mockObserverForStates).onChanged(LoginDataState.ValidCredentialsState)
+        verify(mockObserverForStates, times(2))
+            .onChanged(LoginDataState.Error(ArgumentMatchers.any()))
         verifyNoMoreInteractions(mockObserverForStates)
     }
 
